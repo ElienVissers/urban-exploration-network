25821b2d538c709086b067c5a8258582a91045cf
Elien Vissers <elien.visserssimilon@gmail.com>
2019-01-22 11:58:51 +0100

    upload img to /uploads directory

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const express = require('express');
 const db = require('./db');
 const app = express();
+const multer = require('multer');
+const uidSafe = require('uid-safe');
+const path = require('path');
+
+//takes the uploaded file
+//gives it a unique name of 24 characters (uidSafe)+ the original file extension (path)
+//stores it in our /uploads folder
+var diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    }
+});
+
+//actually doing the uploading
+var uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152
+    }
+});
+
+///////////////////////////////////////////////////////////////////////////////////////
 
 app.use(express.static('./public'));
 
 app.get('/images', (req, res) => {
     db.getImages().then(dbInfo => {
-        console.log("dbInfo: ", dbInfo);
         res.json(dbInfo.rows);
     });
+});
+
+app.post('/upload', uploader.single('uploadedFile'), (req, res) => {
+    console.log('post request to /upload'); //this is not showing yet
+    console.log("req.body: ", req.body);
+    console.log("req.file: ", req.file);
+
+    //next steps: save filename, title, description, name in the images table
+    //make new image render automatically on screen (without reloading the page)
+
 });
 
 app.listen(8080, () => console.log('Listening!'));
--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,13 @@
     new Vue({
         el: '#main',
         data: {
-            images: []
+            images: [],
+            form: {
+                title: '',
+                name: '',
+                description: '',
+                file: null
+            }
         },
         mounted: function() {
             var self = this;
@@ -10,6 +16,24 @@
                 console.log("response from /images to axios: ", response);
                 self.images = response.data;
             });
+        },
+        methods: {
+            uploadFile: function(e) {
+                e.preventDefault();
+                var file = document.getElementById('file');
+                var uploadedFile = file.files[0];
+                // use the formData API to send files to server --> pass it a key value pair with the key as a description and the value the file you wanna send
+                // console.log("this: ", this); /*this refers to the Vue instance*/
+                var formData = new FormData();
+                formData.append('uploadedFile', uploadedFile);
+                formData.append('title', this.form.title);
+                formData.append('description', this.form.description);
+                formData.append('name', this.form.name);
+
+                axios.post('/upload', formData).then(function() {
+
+                });
+            }
         }
     });
 })();
